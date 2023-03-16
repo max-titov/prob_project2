e6f7e6450cb0c228744a6755d25db0d2c17ba3a2
jj mirkovich <hmt6qs@virginia.edu>
2023-03-15 23:25:21 -0400

    Updated names of variables and functions

--- RandomNumberGenerator.py
+++ RandomNumberGenerator.py
@@ -3,17 +3,17 @@ a = 24693
 c = 3517
 K = 2**17
 
-def printNum (x, n):
+def NumGenerator (x, n):
     x1 = (((a*x + c) % K))
     u1 = round(x1/K,4)
     
-    print("Trial Number " + str(n) + ": " + str(u1))
+    print("Random Number " + str(n) + ": " + str(u1))
     if n >= 53:
         return
     else:
-        printNum(x1, n + 1)
+        NumGenerator(x1, n + 1)
 
-printNum(1000, 1)
+NumGenerator(1000, 1)
 
 
 
